4a53c48e436b0ce22a6d8856711d55f7574bfbce
Finn Thye <piqueen@gmail.com>
2019-04-16 15:04:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments) {
   array = [];
+  
+  for (var i = 0; i < musicians.length; i++) {
+    array.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
 }
