9b76c6aca1bc0c9d4e52b5d18f858543f6bfe7b8
Finn Thye <piqueen@gmail.com>
2019-04-16 15:13:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,3 +8,13 @@ function theBeatlesPlay(musicians, instruments) {
   
   return array;
 }
+
+function johnLennonFacts(facts) {
+  var i = 0;
+  while (i<facts.length) do {
+    facts[i] = facts[i] + '!!!';
+    i++;
+  }
+  return facts;
+}
+ 
